5b8ebb22b3fee279b987af35c60a98010964568e
Terry-rose <2216mayet@gmail.com>
2024-01-05 05:22:40 -0800

    commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
-a = 1
-b = 2
-from add_0 import add
+def main():
+    a = 1
+    b = 2
+    from add_0 import add
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    main()
 
