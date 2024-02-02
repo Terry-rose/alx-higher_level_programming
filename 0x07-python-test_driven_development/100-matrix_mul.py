24fa5ac0346f668a887c9fb6d3a49c42a871ffb1
Terry-rose <2216mayet@gmail.com>
2024-02-02 13:14:05 -0800

    commit

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -3,38 +3,37 @@ def matrix_mul(m_a, m_b):
     """
     Multiplies two matrices.
 
-    Parameters:
-    - m_a: a list of lists representing the first matrix
-    - m_b: a list of lists representing the second matrix
+    Args:
+    - m_a (list): The first matrix as a list of lists of integers or floats.
+    - m_b (list): The second matrix as a list of lists of integers or floats.
 
     Returns:
     A new matrix resulting from the multiplication of m_a and m_b.
 
     Raises:
-    - TypeError: if m_a or m_b is not a list, not a list of lists, contains non-integer/float elements, or rows of different sizes
-    - ValueError: if m_a or m_b is empty or if matrices can't be multiplied
+    - ValueError: If m_a or m_b is not a list of lists or is empty.
+    - TypeError: If one element in the matrices is not an integer or float.
+    - ValueError: If m_a and m_b can't be multiplied due to incompatible dimensions.
     """
 
-    # Validate m_a and m_b
     if not isinstance(m_a, list) or not isinstance(m_b, list):
-        raise TypeError("m_a must be a list or m_b must be a list")
+        raise TypeError("m_a must be a list and m_b must be a list")
 
     if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_a must be a list of lists or m_b must be a list of lists")
+        raise TypeError("m_a must be a list of lists and m_b must be a list of lists")
 
-    if not m_a or not all(row for row in m_a) or not m_b or not all(row for row in m_b):
+    if not m_a or not m_b:
         raise ValueError("m_a can't be empty or m_b can't be empty")
 
-    if not all(isinstance(element, (int, float)) for row in m_a for element in row) or not all(isinstance(element, (int, float)) for row in m_b for element in row):
-        raise TypeError("m_a should contain only integers or floats or m_b should contain only integers or floats")
+    if not all(isinstance(element, (int, float)) for row in m_a for element in row):
+        raise TypeError("m_a should contain only integers or floats")
 
-    if any(len(row) != len(m_a[0]) for row in m_a) or any(len(row) != len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_a must be of the same size or each row of m_b must be of the same size")
+    if not all(isinstance(element, (int, float)) for row in m_b for element in row):
+        raise TypeError("m_b should contain only integers or floats")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
-    # Perform matrix multiplication
     result = [[sum(a * b for a, b in zip(row_a, col_b)) for col_b in zip(*m_b)] for row_a in m_a]
 
     return result
