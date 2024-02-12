4104f72b28cb1d8082a22d15e305ef729fb41043
Terry-rose <2216mayet@gmail.com>
2024-02-12 14:00:52 -0800

    commit

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""Module containing the Rectangle class."""
+from models.base import Base
+
+class Rectangle(Base):
+    """Rectangle class, inherits from Base."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Class constructor.
+
+        Args:
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+            x (int): X-coordinate of the rectangle (default is 0).
+            y (int): Y-coordinate of the rectangle (default is 0).
+            id (int): ID of the rectangle (default is None).
+
+        Attributes:
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+            x (int): X-coordinate of the rectangle.
+            y (int): Y-coordinate of the rectangle.
+            id (int): ID of the rectangle.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter for the width attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for the width attribute."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the height attribute."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the height attribute."""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
+
+    @property
+    def x(self):
+        """Getter for the x attribute."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for the x attribute."""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
+
+    @property
+    def y(self):
+        """Getter for the y attribute."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for the y attribute."""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
+
