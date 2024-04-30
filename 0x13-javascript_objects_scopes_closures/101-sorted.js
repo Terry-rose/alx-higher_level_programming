8c3bbfa11adfa2193c85bdc0e0840e2a20068484
Terry_rose <2216mayet@gmail.com>
2024-04-29 17:48:22 -0700

    commit

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const { dict } = require('./101-data');
+
+const occurrencesByUserId = {};
+
+for (const userId in dict) {
+  const occurrences = dict[userId];
+  if (occurrences in occurrencesByUserId) {
+    occurrencesByUserId[occurrences].push(userId);
+  } else {
+    occurrencesByUserId[occurrences] = [userId];
+  }
+}
+
+console.log(occurrencesByUserId);
+
